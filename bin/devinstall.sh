a64bc38855cae672a2db0a93b4361ecae6429b5c
Bryce Hunter <bryce.hunter@hotmail.com>
2017-10-19 21:23:52 +0100

    Set devinstall.sh to update database

--- bin/devinstall.sh
+++ bin/devinstall.sh
@@ -3,3 +3,5 @@
 npm install --only=dev
 composer install
 bower install
+read -p "Please update phinx.yaml and database.php with your database details.  Then press [ENTER] to initalise the database."
+./vendor/bin/phinx migrate
