7a933f80328c059e3a3313d3b57db2c819f2875d
Bryce Hunter <bryce.hunter@hotmail.com>
2017-11-15 14:52:56 +0000

    Corrected path error in grunt file

--- application/views/_partial/header_css_js.php
+++ application/views/_partial/header_css_js.php
@@ -1,8 +1,8 @@
 <?php if ($this->input->cookie('debug') == "true" || $this->input->get("debug") == "true") : ?>
 
     <!-- bower:css -->
-    <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.css" />
-    <link rel="stylesheet" href="bower_components/toastr/toastr.css" />
+    <link rel="stylesheet" href="/bower_components/bootstrap/dist/css/bootstrap.css" />
+    <link rel="stylesheet" href="/bower_components/toastr/toastr.css" />
     <!-- endbower -->
 
     <!-- neptune:css -->
@@ -10,9 +10,9 @@
     <!-- endneptune:css -->
 
     <!-- bower:js -->
-    <script src="bower_components/jquery/dist/jquery.js"></script>
-    <script src="bower_components/bootstrap/dist/js/bootstrap.js"></script>
-    <script src="bower_components/toastr/toastr.js"></script>
+    <script src="/bower_components/jquery/dist/jquery.js"></script>
+    <script src="/bower_components/bootstrap/dist/js/bootstrap.js"></script>
+    <script src="/bower_components/toastr/toastr.js"></script>
     <!-- endbower -->
 
     <!-- neptune:js -->
