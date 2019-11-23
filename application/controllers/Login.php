d03a691b9b84936f802bd404715af18160dfcbae
Bryce Hunter <Bryce Hunter>
2019-11-23 23:50:54 +0000

    Tidy up codebase

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -5,7 +5,6 @@ class Login extends CI_Controller
     public function __construct()
     {
         parent::__construct();
-        $this->output->enable_profiler(TRUE);
     }
 
     public function index()
